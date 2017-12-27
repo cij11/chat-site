9de31b25096e69ba721893ba8e93a7c6856169dc
LAPTOP-47ULR2KJ\chris <chrisjolly25@gmail.com>
2017-12-28 12:34:23 +1300

    Importing lodash. Populating a list with the elements of an array.

--- js/chat.js
+++ js/chat.js
@@ -32,3 +32,18 @@ function postChatLogs() {
     console.log(data.body.generatedNum);
   });
 }
+
+function updateList() {
+  var chatLogArray = ['Tahe', 'Rua', 'Toru', 'Wha'];
+  populateChatLogList(chatLogArray);
+}
+
+function populateChatLogList(chatLogArray) {
+  var chatList = document.getElementById("chatList");
+  _.forEach(chatLogArray, function(logEntry) {
+    var node = document.createElement("LI"); // Create a <li> node
+    var textnode = document.createTextNode(logEntry); // Create a text node
+    node.appendChild(textnode); // Append the text to <li>
+    chatList.appendChild(node);
+  })
+}
