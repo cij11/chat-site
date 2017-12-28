2e26c2575a3e0da8388ab72c08761149192854ab
LAPTOP-47ULR2KJ\chris <chrisjolly25@gmail.com>
2017-12-28 13:27:34 +1300

    Gets the chatLogs from a dynamoDB table called 'chat-table'. Each session in this table consists of an id number (the key) and a list of strings (the chat log entries.)

--- lambdas/getChatLogs.js
+++ lambdas/getChatLogs.js
@@ -1,17 +1,56 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-southeast-1'
+});
+
+/**
+/ To invoke this function via AWS API gateway:
+/ 1. Upload as a lambda function
+/ 2. Create a GET method trigger to this lambda function in API gateway
+/ 3. Enable CORS for the GET method in API gateway
+/ 4. Publish
+
+/ 5. Create dynamoDB table called 'chat-table' with primary key of type
+/ number and name 'id'. Currently pulls this element from the table:
+/ {
+  id: 0
+  logs : ['first', 'second', 'third']
+}
+*/
+
 exports.handler = (event, context, callback) => {
-
   let jsonWrapper = {
-    generatedNum: 10
+    logs: ['Hi', 'Hello', 'Kiaora']
   }
 
-  var response = {
-    "statusCode": 200,
-    "headers": {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-      "Access-Control-Allow-Credentials": true
-    },
-    "body": jsonWrapper
-  }
-  callback(null, response);
+  console.log("Running get from database");
+
+  let getParameters = {
+    TableName: 'chat-table',
+    Key: {
+      // id: parseInt(event.id)
+      id: 0
+    }
+  };
+
+  docClient.get(getParameters, function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      jsonWrapper.logs = data.Item.logs;
+
+      var response = {
+        "statusCode": 200,
+        "headers": {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+          "Access-Control-Allow-Credentials": true
+        },
+        "body": jsonWrapper
+      }
+      callback(null, response);
+    }
+  });
 };
